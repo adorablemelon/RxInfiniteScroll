d89ce0abc329c5ed00e83f822f199594fb3d7e3c
bao <bao.lg1998@gmail.com>
2021-01-13 13:58:25 +0700

    aa

--- BaoRx/ViewController/MainViewController.swift
+++ BaoRx/ViewController/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        print("hmm")
         let service = CustomerServices()
         service.fetchCustomer().subscribe(onNext: {customers in print(customers)}).disposed(by: disposeBag)
         // Do any additional setup after loading the view.
